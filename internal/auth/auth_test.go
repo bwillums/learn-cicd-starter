bef7e50f085620035d4738ac90a36fac4f747e87
Brian <b.willums@gmail.com>
2024-03-25 18:29:19 -0400

    add a test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	var header = http.Header{}
+	header.Add("Authorization", "ApiKey Test")
+	apiKey, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("Error %v", err)
+	}
+
+	if apiKey != "Test" {
+		t.Fatalf("expected: %v, got %v", "Test", apiKey)
+	}
+}
